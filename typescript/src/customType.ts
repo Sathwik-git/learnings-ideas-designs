fa95ba96789e4cbe7329dbaab4af58952929091a
Sathwik-git <sathwikbodakuntla041@gmail.com>
2025-08-27 23:46:56 +0530

    types and interfaces

--- typescript/src/customType.ts
+++ typescript/src/customType.ts
@@ -8,3 +8,40 @@ function greetUser(user: User) {
 }
 
 greetUser({ name: "sathwik", age: 10 });
+
+/* interface Vs type
+implementing interface as a class
+
+types - union | intersection ??
+union => stringOrNumber
+type stringOrNumber = string | number
+let age:stringOrNumber = "1" (correct)
+let age:stringOrNumber = 1  (correct)
+
+intersection => student & intern 
+type employee = student & intern     
+
+*/
+//union
+type age = string | number; 
+
+function checkType(age: age) {}
+
+type student = {
+  name: string;
+  age: number;
+};
+
+type intern = {
+  salary: number;
+  dateofJoining: Date;
+};
+//intersection
+type employee = student & intern;
+
+const e1: employee = {
+  name: "sathwik",
+  age: 10,
+  salary: 100000,
+  dateofJoining: new Date("2005-08-10"),
+};
