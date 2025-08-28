0366fdc966a18567eee2cf427bc1733dea252b3d
Sathwik-git <sathwikbodakuntla041@gmail.com>
2025-08-28 11:15:47 +0530

    implement interface as class

--- typescript/src/customType.ts
+++ typescript/src/customType.ts
@@ -23,7 +23,7 @@ type employee = student & intern
 
 */
 //union
-type age = string | number; 
+type age = string | number;
 
 function checkType(age: age) {}
 
@@ -45,3 +45,55 @@ const e1: employee = {
   salary: 100000,
   dateofJoining: new Date("2005-08-10"),
 };
+
+//interfaces implementation
+
+interface person {
+  name: string;
+  age: number;
+}
+
+class Employe implements person {
+  //can add extra things
+  name: string;
+  age: number;
+
+  constructor(n: string, a: number) {
+    this.age = a;
+    this.name = n;
+  }
+}
+
+const e2 = new Employe("sathwik", 2);
+
+console.log(e2.name);
+console.log(e2.age);
+
+/* diff btw interface and abstract class 
+
+Use abstract class when you want to share common logic or default method implementations.
+
+Use interface when you only want to enforce a shape (structure) without implementation.
+
+*/
+
+abstract class Student {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  abstract greet():  void;
+}
+
+class S1 extends Student {
+  constructor(public name: string, public age: number) {
+    super(name,age)
+  }
+  greet(): void {
+    console.log("hello")
+  };
+}
